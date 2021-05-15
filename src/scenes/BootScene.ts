d64be6d09902eb13d22667d38a7ba14214307b4d
Dragoș Străinu <str.dr4605@gmail.com>
2021-05-15 17:53:37 +0300

    Add phaser example game

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -1,16 +1,24 @@
-import { Scene } from 'phaser'
-import logo from '../assets/logo.png'
+import { Scene } from 'phaser';
+import sky from '../assets/sky.png';
+import ground from '../assets/platform.png';
+import star from '../assets/star.png';
+import bomb from '../assets/bomb.png';
+import dude from '../assets/dude.png';
 
 export default class BootScene extends Scene {
   constructor() {
-    super({ key: 'BootScene' })
+    super({ key: 'BootScene' });
   }
 
   public preload() {
-    this.load.image('logo', logo)
+    this.load.image('sky', sky);
+    this.load.image('ground', ground);
+    this.load.image('star', star);
+    this.load.image('bomb', bomb);
+    this.load.spritesheet('dude', dude, { frameWidth: 32, frameHeight: 48 });
   }
 
   public create() {
-    this.scene.start('PlayScene')
+    this.scene.start('PlayScene');
   }
 }
